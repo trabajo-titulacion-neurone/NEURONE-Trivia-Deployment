d3471300fb9616a1e1ed9921f358cfc8b1dd528a
gerardoluceroc <gerardo.lucero.c@usach.cl>
2023-10-30 01:34:56 -0300

    se arregla problema con nvm
    se realiza un tarea aparte la cual activar la version de node instalada con nvm a traves de un link simbolico, pero queda pendiente el tema del rol mongodb_user ya que la segunda vez que se ejecuta hay problemas de autorizacion y esto es debido a que no esta la linea de login_user y login_password pero el probleme es que si activo estas lineas de codigo, al ejecutarse por primera vez en el servidor tambien saldrá un error debido a que no existe un usuario creado para loguearse por lo que la conexion será refused. Ya que mi objetivo es ejecutar el mismo playbook una y otra vez sin problemas y no separar playbooks de instalacion inicial y otros de actualizacion, probablemente busque hacer algo para verificar si existe algun usuario root dentro de mongo y en caso de que no, este se cree, y en caso de que sí exista, ejecutar los comandos logueandose con este usuario existente.

--- terraform deploy/main.tf
+++ terraform deploy/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 provider "digitalocean" {
-  token = "dop_v1_76d94347f12d7aaad9265faed1df732ecb5228c842bd86f17871541a588d43fa"
+  token = "dop_v1_d1c40f236bfa30db462480632582c1fc38bde6b682f4080ce60a30b688f4d5d9"
 }
 
 resource "digitalocean_droplet" "mi_droplet" {
@@ -21,19 +21,19 @@ resource "digitalocean_droplet" "mi_droplet" {
 }
 
 
-resource "digitalocean_droplet" "mi_droplet_2" {
-  image  = "ubuntu-22-04-x64"
-  name   = "mi-droplet"
-  region = "nyc1"
-  size   = "s-1vcpu-1gb"
-  ssh_keys = ["dd:ca:b2:21:ed:6e:47:dd:12:22:58:c2:58:5d:77:e6"]
-}
+# resource "digitalocean_droplet" "mi_droplet_2" {
+#   image  = "ubuntu-22-04-x64"
+#   name   = "mi-droplet"
+#   region = "nyc1"
+#   size   = "s-1vcpu-1gb"
+#   ssh_keys = ["dd:ca:b2:21:ed:6e:47:dd:12:22:58:c2:58:5d:77:e6"]
+# }
 
-resource "digitalocean_droplet" "mi_droplet_3" {
-  image  = "ubuntu-22-04-x64"
-  name   = "mi-droplet"
-  region = "nyc1"
-  size   = "s-1vcpu-1gb"
-  ssh_keys = ["dd:ca:b2:21:ed:6e:47:dd:12:22:58:c2:58:5d:77:e6"]
-}
+# resource "digitalocean_droplet" "mi_droplet_3" {
+#   image  = "ubuntu-22-04-x64"
+#   name   = "mi-droplet"
+#   region = "nyc1"
+#   size   = "s-1vcpu-1gb"
+#   ssh_keys = ["dd:ca:b2:21:ed:6e:47:dd:12:22:58:c2:58:5d:77:e6"]
+# }
 
