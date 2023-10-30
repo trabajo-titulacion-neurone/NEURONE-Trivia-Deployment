4437c711994c34bdfcecee0818a91b2c9c7de081
gerardoluceroc <gerardo.lucero.c@usach.cl>
2023-10-29 21:44:11 -0300

    Solucion de errores en configuracion de mongo
    se modifcan los roles que se obtuvieron de internet debido a problemas de compatibilidad y otras cosas, por ejemplo en el rol mongodb, si se ejecutaba mas de una vez mongodb se cae en el servidor, esto es debido a la tarea Enable Authentication in MongoDB, ya que en cada ejecucion escribia directamente en el archivo de configuracion sin verificar si la linea a escribir ya existia. Esto ya fue corregido. Tambien se cambia las task del rol mongodb_users ya que utiliza modulos que no estan siendo mantenidos, se cambió por un modulo y una sintaxis que se encuentra actualizada por la comunidad de Ansible. Tambien se cambio en el main.tf de terraform el valor del token de digitalocean debido a que el anterior ya habia vencido. La instalación de mongodb al parecer funciona sin problemas y puede ser ejecutada multiples veces sin alterar ni botar ningun servicio de mala forma. Queda pendiente la instalacion de node con nvm esto debido a que no se ejecutan instrucciones con npm esto debido a que el comando que agregué que hace un link simbolico entre node y nvm al parecer no funcionó, sin embargo no funciona a nivel de las task de ansible, ya que si lo ejecuto directamente desde el servidor el uso de npm vuelve a funcionar sin problemas. El comando que menciono es sudo ln -sf /root/.nvm/versions/node/v16.14.1/bin/node /usr/bin/node

--- terraform deploy/main.tf
+++ terraform deploy/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 provider "digitalocean" {
-  token = "dop_v1_b9f75552f4d4c6681c4a33d694d33b509fb303ed10165a12716960a6882cef4a"
+  token = "dop_v1_84bde58ebdf8c2479386e5e7456a98f958d51947ac61128a2a6a38ced7b60ad3"
 }
 
 resource "digitalocean_droplet" "mi_droplet" {
@@ -19,3 +19,21 @@ resource "digitalocean_droplet" "mi_droplet" {
   size   = "s-1vcpu-1gb"
   ssh_keys = ["dd:ca:b2:21:ed:6e:47:dd:12:22:58:c2:58:5d:77:e6"]
 }
+
+
+resource "digitalocean_droplet" "mi_droplet_2" {
+  image  = "ubuntu-22-04-x64"
+  name   = "mi-droplet"
+  region = "nyc1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = ["dd:ca:b2:21:ed:6e:47:dd:12:22:58:c2:58:5d:77:e6"]
+}
+
+resource "digitalocean_droplet" "mi_droplet_3" {
+  image  = "ubuntu-22-04-x64"
+  name   = "mi-droplet"
+  region = "nyc1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = ["dd:ca:b2:21:ed:6e:47:dd:12:22:58:c2:58:5d:77:e6"]
+}
+
