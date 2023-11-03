98e816bace76f0bf0f38b5199630d35fb18f6136
gerardoluceroc <gerardo.lucero.c@usach.cl>
2023-11-03 01:00:17 -0300

    neuronegm y trivia server corriendo
    Se agrega la tarea en el rol de mongodb_users que agrega al usuario admin de neuroneAdmin para que se conecte neuronegm-server a la base de datos, por lo tanto con esto neuronegm está corriendo sin problemas al principio almenos. Tambien se corre el server de trivia y se genera el compilado de produccion del frontend. Por lo tanto hasta ahora el playbook corre todos sus roles sin problemas, corriendose multiples veces, a excepcion del build de produccion del frontend ya que luego de varios minutos el proceso es finalizado por el sistema operativo, esto es debido a que 1GB de memoria ram no alcanza para generar todos estos procesos. Con 2GB de ram aveces funciona y otras veces no, por lo tanto para esto, se generó un nuevo rol, el cual se llama add-swap-space, el cual crea una especie de particion de intercambio para complementar los 2GB de ram que tiene el servidor y de esta manera el build y el resto de roles corre sin problemas. Esto funcionó solo en servidores desde 2GB de ram como base. A pesar de agregar una particion de 3GB en el servidor de 1GB de ram, tampoco ha funcionado, esto debido al problema mencionado con el agotamiento de la memoria ram

--- terraform deploy/main.tf
+++ terraform deploy/main.tf
@@ -16,18 +16,10 @@ resource "digitalocean_droplet" "mi_droplet" {
   image  = "ubuntu-22-04-x64"
   name   = "mi-droplet"
   region = "nyc1"
-  size   = "s-1vcpu-1gb"
+  size   = "s-1vcpu-2gb"
   ssh_keys = ["dd:ca:b2:21:ed:6e:47:dd:12:22:58:c2:58:5d:77:e6"]
 }
 
-
-# resource "digitalocean_droplet" "mi_droplet_2" {
-#   image  = "ubuntu-22-04-x64"
-#   name   = "mi-droplet"
-#   region = "nyc1"
-#   size   = "s-1vcpu-1gb"
-#   ssh_keys = ["dd:ca:b2:21:ed:6e:47:dd:12:22:58:c2:58:5d:77:e6"]
-# }
 
 # resource "digitalocean_droplet" "mi_droplet_3" {
 #   image  = "ubuntu-22-04-x64"
