9235ba89af3536755218f66b01e2fe14ba59e1f0
gerardoluceroc <gerardo.lucero.c@usach.cl>
2024-02-01 02:15:27 -0300

    Despliegue correcto de Trivia
    Se modificaron los archivos de configuracion de modo de ejecutar Trivia localmente. Debido a que el valor de varias vabriables en los archivos cambian dependiendo de si el depsliegue se realiza en un entorno local o en producción es que se crea el rol GAME-localhost para realizar el despliegue de Trivia en un entorno local, la unica diferencia es el contenido de los archivos de configuración que se encuentran en la carpeta files del rol. El rol original GAME mantiene su nombre y aqui iran los archivos configurados de modo de que el despliegue se haga en producción, osea con certificados SSL y un dominio asignado.

---
+++ ansible-files/roles/GAME-localhost/files/environment.prod.ts
@@ -0,0 +1,28 @@
+/*
+export const environment = {
+  production: true,
+  serverRoot: 'http://159.65.100.191:3030/',
+  apiURL: 'http://159.65.100.191:3030/api/',
+  frontURL: 'http://159.65.100.191:3030/',
+  neuroneURL: 'http://159.65.100.191:3000/',
+  locale: 'es-CL'
+};
+*/
+export const environment = {
+  production: true,
+  serverRoot: 'http://localhost:3030/',
+  apiURL: 'http://localhost:3030/api/',
+  frontURL: 'http://localhost:3030/',
+  neuroneURL: 'http://localhost:3000/',
+  locale: 'es-CL'
+};
+/*
+export const environment = {
+  production: true,
+  serverRoot: 'http://159.89.132.126:3030/',
+  apiURL: 'http://159.89.132.126:3030/api/',
+  frontURL: 'http://159.89.132.126:3030/',
+  neuroneURL: 'http://159.89.132.126:3000/',
+  locale: 'es-CL'
+};
+*/
